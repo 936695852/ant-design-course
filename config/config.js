fdf9d51e165430efce1f4f0debb5af55a4b90492
paooooo <wke1018@gmail.com>
2019-01-03 21:02:38 +0800

    frist commit

--- config/config.js
+++ config/config.js
@@ -1,11 +1,37 @@
 export default {
   singular: true,
   plugins: [
-    ['umi-plugin-react', {
-      // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
-    }],
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: true
+      }
+    ]
   ],
   routes: [
-    { path: '/', component: './HelloWorld' }
+    {
+      path: '/',
+      component: '../layout',
+      routes: [
+        {
+          path: '/',
+          component: 'HelloWorld'
+        },
+        { path: 'puzzlecards', component: './puzzlecards' },
+        {
+          path: 'helloWorld',
+          component: './HelloWorld'
+        },
+        {
+          path: '/dashboard',
+          routes: [
+            { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+            { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+            { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+          ]
+        }
+      ]
+    }
   ]
 }
